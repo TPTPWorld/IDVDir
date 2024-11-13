03d2049712a8dfb41d85ea1580f5cb94854bacfb
Daniel Li <dxl1194@miami.edu>
2024-11-13 13:10:59 -0500

    fixed interestingness

--- IDVApp/webpack.config.js
+++ IDVApp/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "/js/helpers.js",
   output: {
     path: path.resolve(__dirname, 'js'),
-    filename: "IDVBundle.js",
+    filename: "bundle.js",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".css", ".mjs"],
